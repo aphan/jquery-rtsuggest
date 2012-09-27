727329ce6a9a84c9050ef4627263d9c7911cc03b
Adam Phan <aphansh@gmail.com>
2012-09-26 23:24:14 -0700

    show suggestions box

--- jquery.rtsuggest.js
+++ jquery.rtsuggest.js
@@ -57,44 +57,12 @@
       highlightedSuggestion = [];  
     }).addClass( cssClasses.dropdownBox ).appendTo( 'body' ).hide();
 
-    // The dropdown box should change its size accordingly with the position and
-    // size of the input form
-    $( window ).resize( function() {
-      if ( $( dropdownBox ).is( ':visible' ) ) {
-        positionDropdownBox();
-      }
-    });
-    
-    function positionDropdownBox() {
-      $( dropdownBox ).css({
-        position: "absolute",
-        top: $( inputForm ).offset().top + $( inputForm ).outerHeight() - 1.5,
-        left: $(  inputForm ).offset().left,
-        width: $( inputForm ).outerWidth() - 1,
-        'z-index': 40,
-        'font-size': $('#' + inputForm.id).css('font-size')
-    });
-    }
-    
-    // Hide the dropdown when the dropdown is visible and the user clicks anywhere 
-    // besides from the dropdown box or the input form, hide 
-    $( document ).click( function() {
-      useUserInput = true;
-      userInput = $( inputForm ).val();
-      hideDropdownBox();
-    });
-
-    function hideDropdownBox() {
-      dropdownBox.hide();
-      dropdownBox.html( '' );
-    } 
-    
-    // Some action needs to be performed when the user modifies the input of the form 
+    // Perform an action when the user modifies the input of the form 
     $( inputForm ).keyup( function( event ) {
       switch( event.keyCode ) {
         case 38: // up key
         case 40: // down key
-        // Remove whatever dropdown item is highlighted (if any)
+          // Remove whatever dropdown item is highlighted (if any)
           if ( !useUserInput && highlightedSuggestion.length != 0 ) {
             removeHighlight( highlightedSuggestion );
           }
@@ -132,7 +100,7 @@
 
         default:
           if ( String.fromCharCode( event.keyCode ) ) {
-          // Don't do anything on a blank input
+            // Don't do anything on a blank input
             if ( !$(inputForm).val() ) {
               hideDropdownBox();
               break;
@@ -167,7 +135,7 @@
       suggestion.removeClass( cssClasses.highlightedSuggestion );
     }
 
-    
+
     function getSuggestions( query ) {
       // only make server call if cached query doesn't exist
       if ( suggestionsCache[query] != null ) {
@@ -176,28 +144,28 @@
         serverQuery( query );
       }
     }
-    
+
     function serverQuery( query ) {
       var queryUrl = suggestSource + query;
       // If there is a specified URL to get top items (for when the
-      // user clicks onto a blank form), set the query url to that link
-      if (!query && settings.topItemsUrl) {
-        queryUrl = settings.topItemsUrl;
-      } else if ( !query || !(/\S/.test(query)) ) {
-        return;
-      }
-      $.ajax( {
-        url: queryUrl,
-        type: settings.requestType,
-        dataType: settings.dataType,
-        success: function( data ) {
-          formatDropdownBox( data );
-          // cache the results
-          suggestionsCache[query] = data;
+        // user clicks onto a blank form), set the query url to that link
+        if (!query && settings.topItemsUrl) {
+          queryUrl = settings.topItemsUrl;
+        } else if ( !query || !(/\S/.test(query)) ) {
+          return;
         }
-      });
+        $.ajax( {
+          url: queryUrl,
+          type: settings.requestType,
+          dataType: settings.dataType,
+          success: function( data ) {
+            formatDropdownBox( data );
+            // cache the results
+            suggestionsCache[query] = data;
+          }
+        });
     }
-    
+
     function formatDropdownBox( data ) {
       var suggestionsList = $( '<ul />' ).addClass( cssClasses.suggestionsList )
       // selects the nearest dropdown item when the user mouses over the dropdown
@@ -208,8 +176,8 @@
       }).mouseout( function() {
         removeHighlight( highlightedSuggestion );
 
-      // set the input form's text to be the text of the dropdown item that
-      // the user clicks on
+        // set the input form's text to be the text of the dropdown item that
+        // the user clicks on
       }).click( function ( event ) {
         event.stopPropagation();
         var selectedSuggestion = $( event.target ).closest( 'li' );
@@ -220,17 +188,56 @@
       });
 
       $.each(data, function( i, item ) {
-        suggestionsList.append( '<li class="' + cssClasses.suggestionItem 
-        + '"><span class="' + cssClasses.suggestionText + '">' + item + '</span></li>' );
+        suggestionsList.append( '<li class="' + cssClasses.suggestionItem + '"><span class="' 
+        + cssClasses.suggestionText + '">' + item + '</span></li>' );
       });
+
       // Only show the dropdown box if there are suggestions available
       if ( data.length == 0 ) { 
         hideDropdownBox();
       } else {
         dropdownBox.html( suggestionsList );
-        showSuggestions();
+        showDropdownBox();
       }
     }
+
+    function showDropdownBox() {
+      positionDropdownBox();
+      dropdownBox.show();
+    }
+
+    function hideDropdownBox() {
+      dropdownBox.hide();
+      dropdownBox.html( '' );
+    } 
+
+
+    // The dropdown box should change its size accordingly with the position and
+    // size of the input form
+    $( window ).resize( function() {
+      if ( $( dropdownBox ).is( ':visible' ) ) {
+        positionDropdownBox();
+      }
+    });
+
+    function positionDropdownBox() {
+      $( dropdownBox ).css({
+        position: "absolute",
+        top: $( inputForm ).offset().top + $( inputForm ).outerHeight() - 1.5,
+        left: $(  inputForm ).offset().left,
+        width: $( inputForm ).outerWidth() - 1,
+        'z-index': 40,
+        'font-size': $('#' + inputForm.id).css('font-size')
+      });
+    }
+
+    // Hide the dropdown when the dropdown is visible and the user clicks anywhere 
+    // besides from the dropdown box or the input form, hide 
+    $( document ).click( function() {
+      useUserInput = true;
+      userInput = $( inputForm ).val();
+      hideDropdownBox();
+    });
 
   };
 } ( jQuery ));
